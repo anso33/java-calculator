886e1a74235141e0f5c08e5a8596336e203a81a2
anso33 <hyeon131812@gmail.com>
2023-11-07 23:52:41 +0900

    :sparkles: feat: Add split string test

---
+++ src/test/java/string/SplitStringTest.java
@@ -0,0 +1,25 @@
+package string;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SplitStringTest {
+
+	@Test
+	@DisplayName("1,2를 ,로 split하면 1과 2로 나눠진다.")
+	void whenSplitStringByComma_thenSplitString() {
+		String str = "1,2";
+		String[] split = str.split(",");
+		assertThat(split).containsExactly("1", "2");
+	}
+
+	@Test
+	@DisplayName("1을 ,로 split하면 1만 있는 배열이 반환된다.")
+	void whenSplitStringByComma_givenOnlyOne_thenReturnOne() {
+		String str = "1";
+		String[] split = str.split(",");
+		assertThat(split).containsExactly("1");
+	}
+}
